e2954139550687f70692d009aa30628cc586a962
javeed512 <javeedmohammedjaveed@gmail.com>
2026-03-18 17:53:29 +0530

    formiks demo

---
+++ React_Notes_Projects/day4/src_formiks_demo/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Signup } from './Signup';
+
+
+
+function App() {
+   return (
+    <>
+     <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-5">
+         <Signup/>
+        </div>
+       
+      </div>
+     </div>
+    </>
+   
+  );
+
+}
+
+export default App;
---
+++ React_Notes_Projects/day4/src_uncontrolledform_with_bootstrap/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { RegistrationForm } from './RegistrationForm';
+
+function App() {
+  return (
+    <div className="cls1">
+      <RegistrationForm></RegistrationForm>
+
+    </div>
+  );
+}
+
+export default App;
